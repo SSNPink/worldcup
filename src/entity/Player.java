d11fc906cf6e410d9e15e3d228bab19aa245ab3f
石胜男 <@192.168.2.111>
2021-04-20 20:34:33 +0900

    Merge branch 'half'
    Conflicts:
    	src/entity/Game.java
    	src/entity/Player.java
    	src/entity/Team.java

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -2,38 +2,6 @@ package entity;
 
 public class Player {
 	private String name;
-<<<<<<< HEAD
 	private String birthDay;
 	private int goal;
-=======
-	private String tName;
-	private int goals;
-	
-	public Player(String name,String tName){
-		this.name = name;
-		this.tName = tName;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getTName() {
-		return tName;
-	}
-	public void setTName(String tName) {
-		this.tName = tName;
-	}
-	public int getGoals() {
-		return goals;
-	}
-	public void setGoals(int goals) {
-		this.goals = goals;
-	}
-	@Override
-	public String toString() {
-		return name + "-" + tName +  "-" +goals;
-	}
->>>>>>> refs/heads/half
 }
