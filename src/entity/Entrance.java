f5825ebd189bde74bbde0894f2c86acbfffa0a75
石胜男 <@192.168.2.111>
2021-04-20 21:30:01 +0900

    complete2

--- src/entity/Entrance.java
+++ src/entity/Entrance.java
@@ -1,11 +1,5 @@
 package entity;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
